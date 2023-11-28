76f95f1f3b0e868c04ecfa47cbd3091f7e0aa91e
thepoppe <pontus.morin@hotmail.se>
2023-11-28 20:32:08 +0100

    the input textbox used in settings.

---
+++ src/views/settingsInputBox.jsx
@@ -0,0 +1,22 @@
+/**
+ * Creates the input box for the settings, with the current names as default
+ * @param {Object} props - The properties passed to the InputBox component.
+ * @param {string} props.text - The current text to be displayed in the input box.
+ * @param {Function} props.onInputChange - Callback function triggered when the input value changes.
+ * @returns {React.Element} A component with an input box that responds to user input.
+ */
+export default function InputBox(props) {
+  /*ACB to handle inputs*/
+  function handleUserNameInputACB(evt) {
+    props.onInputChange(evt.target.value);
+  }
+  return (
+    <input
+      type="text"
+      value={props.text}
+      onChange={handleUserNameInputACB}
+      className="w-full text-black text-left rounded border border-gray-300
+         p-2 bg-white hover:border-indigo-600 transition duration-300 "
+    />
+  );
+}
