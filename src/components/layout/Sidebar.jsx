3c2921774108778bed173b9da21bba85727a8122
Zia <ziamohseni2013@gmail.com>
2023-11-29 11:32:30 +0100

    Add logo and mobile menu button

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,10 +1,34 @@
 import { Link } from "react-router-dom";
+// Logo
+import logo from "../../assets/firepins-logo.svg";
 
 function Sidebar() {
   return (
-    <div className="bg-pins-secondary text-pins-light w-full md:w-64 p-4">
-      <h2 className="text-lg font-semibold">Sidebar</h2>
-      <nav>
+    <div className="bg-pins-secondary text-pins-light w-full md:w-80">
+      {/* Logo container */}
+      <div className="flex justify-between border-b-2 border-pins-grey-darker md:justify-center">
+        <Link to="/" className="p-4">
+          <img src={logo} alt="FirePins Logo" className="w-48" />
+        </Link>
+        {/* Mobile nav toggle icon */}
+        <button className="bg-pins-light px-6 md:hidden">
+          <svg
+            className="w-8 h-8 text-pins-primary"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="{2}"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </button>
+      </div>
+      {/* Navigation */}
+      <nav className="p-4">
         <ul>
           <li>
             <Link to="/">Home</Link>
