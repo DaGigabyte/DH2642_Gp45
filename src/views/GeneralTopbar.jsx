0fa1ec4a71416deb9baee5701f20c5f4b8d3fc3d
thepoppe <pontus.morin@hotmail.se>
2023-11-30 13:03:22 +0100

    moved TopBar to presenter

--- src/views/GeneralTopbar.jsx
+++ src/views/GeneralTopbar.jsx
@@ -5,10 +5,9 @@ import NewPostButton from "./CreatePostButton";
  * Component for creating new post in top menu bar
  * @param {Object} props - The properties passed to the component
  * @param {String} props.searchText - The current search typed by the user.
- * @param {Function} props.updateUserSearch - CB to update the the searchText
- * @param {Function} props.confirmUserSearch - CB to confirm the search
- * @param {Function} props.createNewPostRequest - CB to navigate to the create new post
- * @param {Function} props.navigateToProfileRequest - CB to navigate to the profile
+ * @param {String} props.profilePicture - The current users profile picture
+ * @param {Function} props.setSearchText - Function to update the search text
+ * @param {Function} props.confirmUserSearch - Function to confirm the search posts
  * @returns {React.Element} A render of the topsearchbar with new post button and profile.
  */
 export default function GeneralTopbar(props) {
@@ -16,20 +15,26 @@ export default function GeneralTopbar(props) {
     <div className="flex flex-col sm:flex-row justify-between space-x-16 pd-10">
       <div className="w-full sm:w-1/2 self-center">
         <Search
-          searchText={props.searchText}
-          onUserTyping={props.updateUserSearch}
+          searchText={props.searchText || ""}
+          onUserTyping={props.setSearchText}
           onUserSearching={props.confirmUserSearch}
         />
       </div>
       <div className="flex space-x-5 justify-end">
         <div className="flex-none self-center ">
-          <NewPostButton onUserClick={props.createNewPostRequest} />
+          <NewPostButton
+            onUserClick={() => {
+              window.location.hash = "#/newPost";
+            }} //TODO change to real routing
+          />
         </div>
         <img
           className="rounded-full self-center h-12 shadow hover:scale-110 transition duration-300"
           src={props.profilePicture}
           alt="profile picture"
-          onClick={props.navigateToProfileRequest}
+          onClick={() => {
+            window.location.hash = "#/Profile";
+          }} //TODO change to real routing
         />
       </div>
     </div>
