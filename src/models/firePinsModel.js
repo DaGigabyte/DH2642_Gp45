33bf1536e8bcbcf7cdb264f2d9e8a3f6c5afb1d0
thepoppe <pontus.morin@hotmail.se>
2023-11-30 02:35:27 +0100

    Update firePinsModel.js
    added the missing fields in the model for the searchfield

--- src/models/firePinsModel.js
+++ src/models/firePinsModel.js
@@ -4,18 +4,25 @@ export default {
     this.count = value;
   },
 
-  fullName: 'My Name',
-  displayName: 'My Nickname',
+  fullName: "My Name",
+  displayName: "My Nickname",
   profilePicture: "default-avatar.jpg",
 
-  setPersonId(id){
-      this.personId = id; 
+  setPersonId(id) {
+    this.personId = id;
   },
-  setFullName(name){
-      this.fullName = name;
+  setFullName(name) {
+    this.fullName = name;
   },
-  setDisplayName(name){
-      this.displayName = name;
+  setDisplayName(name) {
+    this.displayName = name;
   },
-
+  searchText: null,
+  setSearchText(text) {
+    this.searchText = text;
+  },
+  //TODO temporary solution to display custom evt
+  confirmUserSearch() {
+    alert("User wants to search for:  " + this.searchText);
+  },
 };
