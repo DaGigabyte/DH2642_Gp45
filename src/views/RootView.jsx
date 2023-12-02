6448694d03eeecd16d3b8e4a2735b32522068231
Zia <ziamohseni2013@gmail.com>
2023-12-02 12:06:10 +0100

    Dummy topbar

--- src/views/RootView.jsx
+++ src/views/RootView.jsx
@@ -6,8 +6,17 @@ function RootView() {
   return (
     <div className="flex flex-col min-h-screen bg-pins-light md:flex-row">
       <Sidebar />
-      <div className="flex-1 p-4">
-        <Outlet />
+      <div className="flex-1">
+        {/* Sticky top bar */}
+        <div className="sticky top-0 z-10 flex items-center justify-between p-4">
+          <div className="flex items-center space-x-4">
+            <h1 className="text-xl font-semibold text-pins-black">Dashboard</h1>
+          </div>
+        </div>
+        {/* Main content */}
+        <div className="p-4">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
