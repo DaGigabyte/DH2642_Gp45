447c40d526136df5c6c55f48b1b640418560f1e0
Jasper Wong <yom092090@gmail.com>
2023-12-04 17:11:28 +0100

    MobX enforce update state by actions

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 import {connectToFirebase} from "./firebase/firebaseModel.js";
 import settingsReaction from "./components/settings/settingsReaction.js";
 
-configure({ enforceActions: "observed" }); // All state that is observed somewhere needs to be changed through actions. how
+configure({ enforceActions: "observed" }); // All state that is observed somewhere needs to be changed through actions.
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
