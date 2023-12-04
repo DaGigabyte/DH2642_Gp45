e8faa9a80dacc5a137d79370ad27440d562eb3f7
thepoppe <pontus.morin@hotmail.se>
2023-12-05 00:50:22 +0100

    temporary model for hot posts

--- src/presenters/HomePresenter.jsx
+++ src/presenters/HomePresenter.jsx
@@ -2,9 +2,20 @@ import { observer } from "mobx-react-lite";
 import HomePage from "../views/HomePage";
 
 function HomePresenter(props) {
+  const temporary = [
+    { id: 1, cover: 'src/assets/oppenhemier.jpg', source: 'TMdB' },
+    { id: 2, cover: 'src/assets/oppenhemier.jpg', source: 'TMDB' },
+    { id: 3, cover: 'src/assets/oppenhemier.jpg', source: 'TMDB' },
+    { id: 4, cover: 'src/assets/oppenhemier.jpg', source: 'TMDB' },
+    { id: 5, cover: 'src/assets/oppenhemier.jpg', source: 'TMDB' },
+    { id: 6, cover: 'src/assets/oppenhemier.jpg', source: 'TMDB' },
+    { id: 7, cover: 'src/assets/oppenhemier.jpg', source: 'TMDB' },
+    { id: 8, cover: 'src/assets/oppenhemier.jpg', source: 'TMDB' },
+  ];
+
   return (
     <div className="flex flex-col">
-      <HomePage model={props.model} />
+      <HomePage model={props.model} posts={temporary} />
     </div>
   );
 }
