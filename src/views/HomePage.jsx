d1ecc9a7e710032f5056fd68e71df70aacaec75b
thepoppe <pontus.morin@hotmail.se>
2023-12-05 16:58:42 +0100

    conditional rendering for the whole slider div

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -9,6 +9,22 @@ import { MdChevronLeft, MdChevronRight } from "react-icons/md"
  * @returns {React.Element} A render of the homepage
  */
 function HomePage(props) {
+
+  function renderTopRatedSection(){
+    if (props.hotPosts.length < 1)
+      return null;
+    return (
+      <div className="flex  items-center h-[400px] m-5 mb-20"> {/*outer*/}
+        <MdChevronLeft onClick={slideLeft} size="30"
+          className="h-full w-8  shrink-0 flex items-center cursor-pointer rounded-lg shadow hover:bg-gray-300 hover:shadow-lg transition duration-200" />
+        <div id="slider"
+          className="p-5 items-center overflow-x-scroll overflow-y-visible whitespace-nowrap scroll-smooth scrollbar-hide"> {/*inner*/}
+          {props.hotPosts.map(renderHotPosts)}
+          </div>
+        <MdChevronRight onClick={slideRight} size="30"
+        className="h-full w-8 shrink-0 flex items-center cursor-pointer rounded-lg shadow hover:bg-gray-300 hover:shadow-lg transition duration-200 ml-2" />
+      </div> )
+  }
   function renderHotPosts(post) {
     return <TopRatedCard key={post.postId} postPicture={post.posterPath} source={post.source} />
   }
@@ -27,19 +43,7 @@ function HomePage(props) {
     <div className="flex flex-col w-full xl:w-3/4">
       {/*Top rated pins*/}
       <span className="text-3xl font-semibold block m-5">Top Rated Pins</span>
-       <div className="flex  items-center h-[400px] m-5 mb-20"> {/*outer*/}
-
-        <MdChevronLeft onClick={slideLeft} size="30"
-          className="h-full w-8  shrink-0 flex items-center cursor-pointer rounded-lg shadow hover:bg-gray-300 hover:shadow-lg transition duration-200" />
-
-        <div id="slider"
-          className="p-5 items-center overflow-x-scroll overflow-y-visible whitespace-nowrap scroll-smooth scrollbar-hide"> {/*inner*/}
-          {props.hotPosts ? props.hotPosts.map(renderHotPosts) : null}
-        </div>
-
-        <MdChevronRight onClick={slideRight} size="30"
-          className="h-full w-8 shrink-0 flex items-center cursor-pointer rounded-lg shadow hover:bg-gray-300 hover:shadow-lg transition duration-200 ml-2" />
-      </div>
+       {renderTopRatedSection()}
       {/*NEW POSTS*/}
       <span className="text-3xl font-semibold  m-5">New Posts</span>
       <div className="flex flex-col w-full m-5">
