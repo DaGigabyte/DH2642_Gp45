9be22024b5f64cccc16d357805fba6dfd5303b62
thepoppe <pontus.morin@hotmail.se>
2023-12-05 02:03:42 +0100

    new posts added to the homepage

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,4 +1,5 @@
 import TopRatedCard from "../components/homepage/TopRatedCard"
+import Post from "./summarizedPost"
 import { SlArrowLeft, SlArrowRight } from "react-icons/sl"; //chagne to this?
 import { MdChevronLeft, MdChevronRight } from "react-icons/md"
 /**
@@ -7,7 +8,7 @@ import { MdChevronLeft, MdChevronRight } from "react-icons/md"
  * @returns {React.Element} A render of the homepage
  */
 function HomePage(props) {
-  function renderPosts(post) {
+  function renderHotPosts(post) {
     return <TopRatedCard key={post.id} postPicture={post.cover} source={post.source} />
   }
   function slideLeft() {
@@ -18,26 +19,34 @@ function HomePage(props) {
     const slider = document.getElementById("slider");
     slider.scrollLeft = slider.scrollLeft + 200;
   }
+
+  function renderNewPosts(post){
+    return<Post key={post.postId} picture={post.picture} nickName={post.nickName} postPicture={post.postPicture}/>
+  }
   return (
-    <div className="flex flex-col w-full">
+    <div className="flex flex-col w-3/4">
       {/*Top rated pins*/}
-      <span className="text-3xl font-semibold pb-4 block pd-10 mt-2">Top Rated Pins</span>
+      <span className="text-3xl font-semibold block m-5">Top Rated Pins</span>
       <div className="flex  items-center h-[400px] m-5 mb-20"> {/*outer*/}
 
         <MdChevronLeft onClick={slideLeft} size="30"
-          class="h-full w-28 flex items-center cursor-pointer rounded-lg shadow hover:shadow-xl transition duration-200" />
+          className="h-full w-8  shrink-0 flex items-center cursor-pointer rounded-lg shadow hover:shadow-xl transition duration-200" />
 
         <div id="slider"
           className="p-5 items-center overflow-x-scroll overflow-y-visible whitespace-nowrap scroll-smooth scrollbar-hide"> {/*inner*/}
-          {props.posts ? props.posts.map(renderPosts) : null}
+          {props.hotPosts ? props.hotPosts.map(renderHotPosts) : null}
         </div>
 
         <MdChevronRight onClick={slideRight} size="30"
-          class="h-full w-28 flex items-center cursor-pointer rounded-lg shadow hover:shadow-xl transition duration-200 ml-2" />
+          className="h-full w-8 shrink-0 flex items-center cursor-pointer rounded-lg shadow hover:shadow-xl transition duration-200 ml-2" />
       </div>
-      <div>
-      {/*More content*/}
+      {/*NEW POSTS*/}
+      <span className="text-3xl font-semibold  m-5">New Posts</span>
+      <div className="flex flex-col w-full m-5">
+      {props.newPosts ? props.newPosts.map(renderNewPosts) : null}
+
       </div>
+      {/*More Content*/}
       <div>
         <h1 className="text-2xl font-bold">HomePage</h1>
         <p>Main content for HomePage...</p>
