d11bfe2999aade5c3b98edf8c43b38daf62befc9
thepoppe <pontus.morin@hotmail.se>
2023-12-05 02:16:06 +0100

    manual scaling 1%

--- src/components/homepage/TopRatedCard.jsx
+++ src/components/homepage/TopRatedCard.jsx
@@ -11,8 +11,8 @@ export default function TopRatedCard(post) {
       onClick={() => { alert("NAVIGATE TO POST") }}//TODO correct routing need postmodel
       src={post.postPicture}
       alt="Post Cover"
-      className="h-[400px] aspect-[1/2] p-2 mr-3 inline-block  cursor-pointer rounded-2xl 
-        shadow object-cover hover:scale-105 hover:shadow-lg ease-in-out transition duration-300 "
+      className="h-[400px] aspect-[1/2]  mr-3 inline-block  cursor-pointer rounded-2xl 
+        shadow object-cover hover:scale-[101%] hover:shadow-lg transition duration-300 "
     />
   );
 }
