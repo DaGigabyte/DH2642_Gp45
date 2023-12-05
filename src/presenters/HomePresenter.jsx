4b34e3cee0007ce58d90af51d0914c290679bd8b
thepoppe <pontus.morin@hotmail.se>
2023-12-05 02:03:22 +0100

    added dummy model for new posts

--- src/presenters/HomePresenter.jsx
+++ src/presenters/HomePresenter.jsx
@@ -2,6 +2,7 @@ import { observer } from "mobx-react-lite";
 import HomePage from "../views/HomePage";
 
 function HomePresenter(props) {
+
   const temporary = [
     { id: 1, cover: 'src/assets/oppenhemier.jpg', source: 'TMdB' },
     { id: 2, cover: 'src/assets/oppenhemier.jpg', source: 'TMDB' },
@@ -12,10 +13,16 @@ function HomePresenter(props) {
     { id: 7, cover: 'src/assets/oppenhemier.jpg', source: 'TMDB' },
     { id: 8, cover: 'src/assets/oppenhemier.jpg', source: 'TMDB' },
   ];
+  const moreTemp = [
+    {postid:9, picture:'public/default-avatar.jpg', nickName: "some name", postPicture:'src/assets/avatar_movie.jpg'},
+    {postid:9, picture:'public/default-avatar.jpg', nickName: "some name", postPicture:'src/assets/avatar_movie.jpg'},
+    {postid:9, picture:'public/default-avatar.jpg', nickName: "some name", postPicture:'src/assets/avatar_movie.jpg'},
+    {postid:9, picture:'public/default-avatar.jpg', nickName: "some name", postPicture:'src/assets/avatar_movie.jpg'},
+  ]
 
   return (
     <div className="flex flex-col">
-      <HomePage model={props.model} posts={temporary} />
+      <HomePage model={props.model} hotPosts={temporary} newPosts={moreTemp}/>
     </div>
   );
 }
