17033dd63d549c4264f16835edcad13f0c771672
thepoppe <pontus.morin@hotmail.se>
2023-12-06 21:06:03 +0100

    arrows now correct size again

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -15,15 +15,17 @@ function HomePage(props) {
     if (props.hotPosts.length < 1)
       return null;
     return (
-      <div className="flex items-center h-[400px] p-5 mb-20"> {/*outer*/}
+      <div className="flex items-center h-[400px] pl-5 pr-5 mb-20"> {/*outer*/}
         <MdChevronLeft onClick={slideLeft} size="30"
-          className="h-full w-8  shrink-0 flex items-center cursor-pointer rounded-lg shadow hover:bg-gray-300 hover:shadow-lg transition duration-200" />
+          className="h-full w-8 shrink-0 flex items-center cursor-pointer rounded-s-lg
+          border-gray-300 border-solid border hover:bg-gray-300 transition ease-in duration-300" />
         <div id="slider"
-          className="p-5 items-center overflow-x-scroll overflow-y-visible whitespace-nowrap scroll-smooth scrollbar-hide"> {/*inner*/}
+          className="items-center overflow-x-scroll overflow-y-visible whitespace-nowrap scroll-smooth scrollbar-hide"> {/*inner*/}
           {props.hotPosts.map(renderHotPosts)}
         </div>
         <MdChevronRight onClick={slideRight} size="30"
-          className="h-full w-8 shrink-0 flex items-center cursor-pointer rounded-lg shadow hover:bg-gray-300 hover:shadow-lg transition duration-200 ml-2" />
+          className="h-full w-8 shrink-0 flex items-center cursor-pointer rounded-e-lg
+          border-gray-300 border-solid border hover:bg-gray-300 transition ease-in duration-300" />
       </div>)
   }
   function renderHotPosts(post) {
@@ -46,7 +48,7 @@ function HomePage(props) {
       <span className="text-3xl font-semibold block m-5">Top Rated Pins</span>
       {renderTopRatedSection()}
       {/*NEW POSTS*/}
-      <span className="text-3xl font-semibold  m-5">New Posts</span>
+      <span className="text-3xl font-semibold  p-5">New Posts</span>
       <div className="flex flex-col w-full p-5">
         {props.newPosts ? props.newPosts.map(renderNewPosts) : null}
       </div>
