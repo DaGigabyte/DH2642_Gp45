bfbb4884887a6b73c9e179e3eb7f2abd6b01ef0a
Zia <ziamohseni2013@gmail.com>
2023-12-07 11:06:04 +0100

    Implemented Swiper component and some other miner adjustments

--- src/components/modal/CreateNewPost.jsx
+++ src/components/modal/CreateNewPost.jsx
@@ -1,3 +1,13 @@
+import { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// import required modules
+import { FreeMode, Pagination } from "swiper/modules";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
 /**
  * @param {function} props.setSearchTextTMDB set search text for TMDB
  * @param {array} props.searchResultsTMDB search results array for TMDB
@@ -10,7 +20,7 @@ function CreateNewPost(props) {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col">
       <input
         type="text"
         placeholder="Search for a movie"
@@ -18,23 +28,33 @@ function CreateNewPost(props) {
         className="border-2 border-gray-300 rounded-lg p-2 m-2 w-full"
         onChange={handleSearchInputChange}
       />
-      <div className="flex items-start space-x-2 scroll-m-2">
-        {props.searchResultsTMDB &&
-          props.searchResultsTMDB.map((result) => {
-            return (
-              <div
-                key={result.id}
-                className="flex flex-col items-center justify-center w-32"
-              >
-                <img
-                  src={`https://image.tmdb.org/t/p/original/${result.poster_path}`}
-                  alt={result.title}
-                  className="w-32 h-48 rounded-lg"
-                />
-                <p className="text-center">{result.title}</p>
-              </div>
-            );
-          })}
+      <div>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="mySwiper"
+        >
+          {props.searchResultsTMDB &&
+            props.searchResultsTMDB.map((result) => {
+              return (
+                <SwiperSlide key={result.id}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/original/${result.poster_path}`}
+                    alt={result.title}
+                    className="w-32 h-48 rounded-lg object-cover"
+                  />
+                  <p className="text-center text-ellipsis overflow-hidden">
+                    {result.title}
+                  </p>
+                </SwiperSlide>
+              );
+            })}
+        </Swiper>
       </div>
     </div>
   );
