f27261f824dd558a75f335c3d74f599cceae25eb
thepoppe <pontus.morin@hotmail.se>
2023-12-08 15:16:31 +0100

    conditionalRender for details

--- src/presenters/DetailsPresenter.jsx
+++ src/presenters/DetailsPresenter.jsx
@@ -2,13 +2,14 @@ import { observer } from "mobx-react-lite";
 import DetailPostView from "../views/DetailPostView";
 
 function DetailsPresenter(props){
-
-    
-  return (
-    <div className="flex flex-col">
-      <DetailPostView post={{}} /> {/*post={props.model.getCurrentPost()*/}
-    </div>
-  );
+  
+  function verifyCurrentPost(){
+    const post=props.model.homePageData.getCurrentPost();
+    if (post===null)
+      return <h1>Post not found</h1>
+    return <DetailPostView post={post} />
+  }
+  return ( verifyCurrentPost() );
 }
 
 export default observer(DetailsPresenter);
