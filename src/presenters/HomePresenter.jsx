95db5ad1fb732eb184508c41c09d48a87b46e930
thepoppe <pontus.morin@hotmail.se>
2023-12-08 15:17:44 +0100

    loading posts implemented

--- src/presenters/HomePresenter.jsx
+++ src/presenters/HomePresenter.jsx
@@ -1,21 +1,25 @@
 import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
 import HomePage from "../views/HomePage";
 
 function HomePresenter(props) {
-  let data = props.model.homePageData.data
+  let data = props.model.homePageData.data;
+
   function loadMorePostACB() {
     props.model.homePageData.fetchNewestPosts();
   }
-
-
-
-  function userSelectsPost(postId){
-    
+  function userSelectsPost(postId) {
+    props.model.homePageData.setCurrentPostID(postId);
   }
-
+  useEffect(() => { loadMorePostACB(); }, [])
   return (
     <div className="flex flex-col">
-      <HomePage hotPosts={data.topRatedPosts} newPosts={data.newestPosts} loadMorePosts={loadMorePostACB} />
+      <HomePage
+        hotPosts={data.topRatedPosts}
+        newPosts={data.newestPosts}
+        loadMorePosts={loadMorePostACB}
+        selectPost={userSelectsPost}
+        loading={props.model.homePageData.loading} />
     </div>
   );
 }
