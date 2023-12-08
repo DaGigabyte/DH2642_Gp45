141f1bac5a5448e4390c8b9b5a0a0d78e6c5961f
thepoppe <pontus.morin@hotmail.se>
2023-12-08 01:50:25 +0100

    now using real posts

--- src/presenters/HomePresenter.jsx
+++ src/presenters/HomePresenter.jsx
@@ -2,38 +2,14 @@ import { observer } from "mobx-react-lite";
 import HomePage from "../views/HomePage";
 
 function HomePresenter(props) {
-
-  /*Source only needed if we use multiple APIs and want the badge*/
-  const temporary = [
-    { postId: 1, posterPath: 'src/assets/oppenhemier.jpg', source: 'TMdB' },
-    { postId: 2, posterPath: 'src/assets/oppenhemier.jpg', source: 'TMDB' },
-    { postId: 3, posterPath: 'src/assets/oppenhemier.jpg', source: 'TMDB' },
-    { postId: 4, posterPath: 'src/assets/oppenhemier.jpg', source: 'TMDB' },
-    { postId: 5, posterPath: 'src/assets/oppenhemier.jpg', source: 'TMDB' },
-    { postId: 6, posterPath: 'src/assets/oppenhemier.jpg', source: 'TMDB' },
-    { postId: 7, posterPath: 'src/assets/oppenhemier.jpg', source: 'TMDB' },
-    { postId: 8, posterPath: 'src/assets/oppenhemier.jpg', source: 'TMDB' },
-  ];
-  /*for */
-  const moreTemp = [
-    {postId:9, profilePicture:'public/default-avatar.jpg', createdBy: "some name 1", posterPath:'src/assets/avatar_movie.jpg',  title:'Title 1', 
-    content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt alias,harum debitis officiis odit id impedit nostrum, sed itaque non pudiandae odio, ipsum mollitia laudantium cum. At earum natus quis?'},
-    {postId:10, profilePicture:'public/default-avatar.jpg', createdBy: "some name 2", posterPath:'src/assets/avatar_movie.jpg', title:'Title 2',
-    content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt alias,harum debitis officiis odit id impedit nostrum, sed itaque non pudiandae odio, ipsum mollitia laudantium cum. At earum natus quis?'},
-    {postId:11, profilePicture:'public/default-avatar.jpg', createdBy: "some name 3", posterPath:'src/assets/avatar_movie.jpg', title:'Title 3',
-    content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt alias,harum debitis officiis odit id impedit nostrum, sed itaque non pudiandae odio, ipsum mollitia laudantium cum. At earum natus quis?'},
-    {postId:12, profilePicture:'public/default-avatar.jpg', createdBy: "some name 4", posterPath:'src/assets/avatar_movie.jpg', title:'Title 4',
-    content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt alias,harum debitis officiis odit id impedit nostrum, sed itaque non pudiandae odio, ipsum mollitia laudantium cum. At earum natus quis?'},
-  ]
-
-  function loadMorePostACB(){
-    //TODO load more post into newPosts array
-    alert("USER WANTS MORE POSTS TODO")
+  let data = props.model.homePageData.data
+  function loadMorePostACB() {
+    props.model.homePageData.fetchNewestPosts();
   }
 
   return (
     <div className="flex flex-col">
-      <HomePage model={props.model} hotPosts={temporary} newPosts={moreTemp} loadMorePosts={loadMorePostACB}/>
+      <HomePage hotPosts={data.topRatedPosts} newPosts={data.newestPosts} loadMorePosts={loadMorePostACB} />
     </div>
   );
 }
