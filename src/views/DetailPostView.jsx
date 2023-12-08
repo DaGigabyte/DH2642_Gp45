2c604c2405ae7c1813faac0c73ef879f206205c1
thepoppe <pontus.morin@hotmail.se>
2023-12-08 15:18:10 +0100

    display default avatar if profilepicture unavailable

--- src/views/DetailPostView.jsx
+++ src/views/DetailPostView.jsx
@@ -1,18 +1,17 @@
 import ProfileBox from "./profilePicAndNick"
 import ReturnButton from "../components/navigation/ReturnButton.jsx"
+import defaultPic from "../assets/default-avatar.jpg"
 /**
  * Renders the Detailed post view
  * @param {Object} props.post - The post to display
  * @returns {React.Element} A render of the post
  */
 function DetailedPostView(props) {
-    if(!props)
-        return null;
     return (  
     <div className="flex flex-col gap-2  w-full max-w-6xl">
         <div className="relative bg-white rounded-2xl">
             <ReturnButton size="30"/>
-            <div className="flex flex-col xl:block p-20 ">
+            <div className="flex flex-col xl:block p-10 ">
 
                 <div className="float-left h-[400px] shrink-0 rounded-xl mr-10 mb-10 overflow-hidden">
                     <img className="w-full h-full object-cover 2xl:object-contain " src={props.post.posterPath} alt="/"/>
@@ -20,8 +19,8 @@ function DetailedPostView(props) {
                 <div className="clear-right">
                     <span className="block text-3xl mt-4 ">
                         <ProfileBox 
-                        picture={props.post.profilePicture} 
-                        nick = {props.post.createdBy}/>
+                        picture={props.post.user.profilePicture? props.post.user.profilePicture: defaultPic} 
+                        nick = {props.post.user.displayName}/>
                     </span>
                     <span className="block text-3xl mt-6 mb-2">{props.post.title}</span>
                     <div className="text-lg text-justify">
