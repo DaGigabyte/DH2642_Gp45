4cd76c89bc266d66279cb1578dc179cbade6a3c6
thepoppe <pontus.morin@hotmail.se>
2023-12-12 22:25:21 +0100

    user interface buttons added to posts

--- src/presenters/HomePresenter.jsx
+++ src/presenters/HomePresenter.jsx
@@ -1,25 +1,35 @@
 import { observer } from "mobx-react-lite";
 import { useEffect } from "react";
+import CommentModal from './CommentModal';
 import HomePage from "../views/HomePage";
 
 function HomePresenter(props) {
   let data = props.model.homePageData.data;
+  useEffect(() => { loadMorePostACB(); }, [])
 
   function loadMorePostACB() {
     props.model.homePageData.fetchNewestPosts();
   }
-  function userSelectsPost(postId) {
+  function userSelectsPostACB(postId) {
     props.model.homePageData.setCurrentPostID(postId);
   }
-  useEffect(() => { loadMorePostACB(); }, [])
+  function userlikesPostACB(postId) {
+    alert('User likes post'+postId)
+  }
+  function userdislikesPostACB(postId) {
+    alert('User dislikes post'+postId)
+  }
   return (
     <div className="flex flex-col">
       <HomePage
+        currentUser={props.model.user}
         hotPosts={data.topRatedPosts}
         newPosts={data.newestPosts}
         loadMorePosts={loadMorePostACB}
-        selectPost={userSelectsPost}
-        loading={props.model.homePageData.loading} />
+        selectPost={userSelectsPostACB} 
+        likePost={userlikesPostACB}
+        dislikePost={userdislikesPostACB}
+        />
     </div>
   );
 }
--- src/views/summarizedPost.jsx
+++ src/views/summarizedPost.jsx
@@ -1,32 +1,63 @@
-import ProfileBox from "./profilePicAndNick";
-
+import { BiLike, BiDislike, BiCommentDetail  } from "react-icons/bi";
+import { useNavigate } from 'react-router-dom';
 /**
  * A general component for post diplaying a preview
  * @param {Object} props - Props for the SummarizedPost component.
  * @param {string} props.picture - The URL or source for the profile picture.
  * @param {string} props.nickName - The nickname associated with the post.
  * @param {int}    props.postId - The id of the post
+ * @param {int}    props.nofLikes - The nof likes on the post
+ * @param {int}    props.nofDislikes - The nof dislikes on the post
+ * @param {boolean}    props.currentUserLikes - True if the user has liked
+ * @param {boolean}    props.currentUserDislikes - True if the user has disliked
  * @param {string} props.postPicture - The URL or source for the post picture.
  * @param {string} props.postTitle - The URL or source for the post picture.
  * @param {string} props.postBody - The URL or source for the post picture.
  * @param {Function} props.viewPost - CB function triggered when the user clicks on the post.
+ * @param {Function} props.dislikePost - CB function triggered when the user clicks the dislike button
+ * @param {Function} props.likePost - CB function triggered when the user clicks the like button.
  * @returns {React.Element} A component displaying a summarized post preview.
  */
 export default function SummarizedPost(props) {
-
-  /*ACB to handle postclick navigate ROUTING NEEEDED*/
+  const navigate = useNavigate();
+  /*OnClick ACBs*/
   function handlePostClickACB() {
-    props.selectPost();//TODO correct routing
+    props.selectPost();
+    navigate('/details');
   }
+  function handleCommentClickACB(event){
+    event.stopPropagation();
+    props.commentPost();
+  }
+  function handleDislikeClickACB(event){
+    event.stopPropagation();
+    props.dislikePost();
+  }
+  function handleLikeClickACB(event){
+    event.stopPropagation();
+    props.likePost();
+  }
+  function checkLikeColor(){
+    if(props.currentUserLike)
+      return "#8D66FC";
+    else return "black";
+  }
+  function checkDislikeColor(){
+    if(props.currentUserDislikes)
+      return "#8D66FC";
+    else return "black";
+  }
+  /* React Component*/
   return (
     <div
-      className="flex flex-col xl:flex-row items-start space-x-4 w-full text-black text-left 
-        rounded-xl border border-gray-300 p-2 bg-white shadow hover:shadow-xl cursor-pointer 
+      className="relative flex flex-col xl:flex-row space-x-4 w-full text-black text-left 
+          rounded-xl border border-gray-300 p-3 bg-white shadow hover:shadow-xl cursor-pointer 
              hover:bg-gray-200 transition duration-300 mb-5"
       onClick={handlePostClickACB}
+      title="Click to view post"
     >
-      <div className="shrink-0  aspect-video xl:aspect-square max-h-[300px] overflow-hidden rounded-xl">
-        {/**Exchange for real data from post */}
+      {/* Image container */}
+      <div className="shrink-0 aspect-video w-full xl:w-auto max-h-[300px] overflow-hidden rounded-xl">
         <img
           src={props.postPicture}
           alt="Picture"
@@ -34,16 +65,38 @@ export default function SummarizedPost(props) {
         />
       </div>
 
-      <div className="flex flex-col mt-4 gap-2">
-        <ProfileBox picture={props.picture} nick={props.nickName} />
-        <span className="text-2xl flex-shrink-0 font-semibold">
+      {/* Text container */}
+      <div className="flex flex-col pl-3 pt-6 pr-3 w-full">
+
+        {/* Profile and name */}
+        <div className="flex items-center pb-4 space-x-4">
+          <span className="mr-2">
+            <img src={props.picture} alt="profile picture" className="w-16 h-16 rounded-full" />
+          </span>
+          <span className="text-3xl">{props.nickName}</span>
+        </div>
+
+        {/* Post Content */}
+        <span className="text-2xl line-clamp-1 font-semibold">
           {props.postTitle}
         </span>
         <span className="text-lg line-clamp-3 font-normal">
           {props.postBody}
         </span>
 
-        {/* Buttons added later*/}
+        {/* LikeButtons  xl:absolute xl:bottom-5 xl:right-10*/}
+        <div className="flex justify-end items-center mt-auto">
+          <div title="Click to comment" onClick={handleCommentClickACB} className="postModifyingButtons"><BiCommentDetail size="40"/></div>
+          <div className="pl-5 text-s font-light">dislikes: {props.nofDislikes}</div>
+          <div title="Click to dislike" onClick={handleDislikeClickACB} className="postModifyingButtons"><BiDislike size="40" color={checkDislikeColor()}/></div>
+          <div className="pl-5 text-s font-light">likes: {props.nofLikes}</div>
+          <div title="Click to like" onClick={handleLikeClickACB} className="postModifyingButtons"><BiLike size="40" color={checkLikeColor()} /></div>
+        </div>
+      </div>
+
+      {/* Source Badge */}
+      <div className="absolute top-0 right-0 rounded-tr-xl rounded-bl-xl overflow-hidden">
+        <div className="bg-pins-primary py-2 px-8 text-xl text-white"> TMDB</div>
       </div>
     </div>
   );
