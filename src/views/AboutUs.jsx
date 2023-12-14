d295c74735c39e5b89a4cf43b7edac1f78a87e0e
Zia <ziamohseni2013@gmail.com>
2023-12-14 21:50:55 +0100

    Add about-the-team text in about page

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -1,8 +1,23 @@
 function AboutUs() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold">AboutUs</h1>
-      <p>Main content for AboutUs...</p>
+    <div className="max-w-6xl">
+      {/* About the team */}
+      <div className="flex flex-col items-center space-y-10">
+        <h1>About The Team</h1>
+        <p className="text-lg px-5">
+          Welcome! We are four Computer Science students from KTH University
+          working on an exciting project for our course, "DH2642 HT23
+          Interaction Programming and the Dynamic Web (iprogdh)." Our goal is to
+          create an advanced website that connects with other online services
+          using APIs, a cornerstone in modern web development. We're focusing on
+          using Firebase to store our website's information safely and
+          efficiently. This project is all about combining our classroom
+          learning with real-world web development, making a website that's not
+          only functional but also engaging. Join us on this journey as we
+          explore the world of web development and bring our classroom theories
+          to life!
+        </p>
+      </div>
     </div>
   );
 }
