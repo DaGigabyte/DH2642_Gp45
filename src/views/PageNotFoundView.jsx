a71105463ccde5f0ec25c58478ce86ea5d243a57
Zia <ziamohseni2013@gmail.com>
2023-12-22 11:08:11 +0100

    Add bg to page not found view, looked strange after centering page content

--- src/views/PageNotFoundView.jsx
+++ src/views/PageNotFoundView.jsx
@@ -5,7 +5,7 @@ import disconnected from "../assets/404/disconnected.png";
 
 function PageNotFoundView() {
   return (
-    <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16 h-screen">
+    <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16 h-screen bg-pins-light">
       <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
         <div className="relative">
           <div className="absolute">
