4ded057405f9c81a97e97d4b4124124cd4be31b5
thepoppe <pontus.morin@hotmail.se>
2023-12-22 16:26:51 +0100

    Lay out from top rated view added. Temporary displaying the wrong array

--- src/presenters/FavoritesPresenter.jsx
+++ src/presenters/FavoritesPresenter.jsx
@@ -1,10 +1,78 @@
 import { observer } from "mobx-react-lite";
 import FavoritesView from "../views/FavoritesView";
+import { useEffect, useState } from "react";
+import {
+  newCommentCreatedToast,
+  newCommentFailedToast,
+} from "../utils/toastify.js";
+import SuspenseAnimation from "../components/global/SuspenseAnimation.jsx";
 
 function FavoritesPresenter(props) {
+  const posts = props.model.favoritesPageData.favoritePosts;
+  console.log("FAVORITES", posts);
+
+  const topRatedPosts = props.model.topRatedPostsData.topRatedPosts;
+  const [commentModalOpen, setCommentModalOpen] = useState(false);
+  const commentStatus = props.model.postDetailData.postCommentStatus;
+
+  useEffect(() => {
+    props.model.favoritesPageData.fetchFavoritePosts();
+  }, []);
+
+  useEffect(() => {
+    if (commentStatus === "success") {
+      newCommentCreatedToast();
+      setCommentModalOpen(false);
+      props.model.postDetailData.setPostCommentStatus(null);
+    } else if (commentStatus === "error") {
+      newCommentFailedToast();
+      setCommentModalOpen(false);
+      props.model.postDetailData.setPostCommentStatus(null);
+    }
+  }, [commentStatus, props.model.postDetailData]);
+
+  function userSelectsPostACB(postId) {
+    props.model.postDetailData.setCurrentPostID(postId);
+  }
+
+  function userlikesPostACB(postId) {
+    props.model.postDetailData.setCurrentPostID(postId);
+    props.model.postDetailData.likePost();
+    props.model.topRatedPostsData.fetchTopPosts();
+  }
+
+  function userdislikesPostACB(postId) {
+    props.model.postDetailData.setCurrentPostID(postId);
+    props.model.postDetailData.dislikePost();
+    props.model.topRatedPostsData.fetchTopPosts();
+  }
+
+  function handleSubmittedCommentACB(post) {
+    props.model.postDetailData.setCurrentPostID(post.id);
+    props.model.postDetailData.postComment();
+    props.model.postDetailData.setComment("");
+  }
+
+  // Show suspense while fetching data
+  if (topRatedPosts.length === 0) {
+    return <SuspenseAnimation loading={true} />;
+  }
+
   return (
     <div className="flex flex-col">
-      <FavoritesView model={props.model} />
+      <FavoritesView
+        currentUID={props.model.user.uid}
+        posts={topRatedPosts}
+        selectPost={userSelectsPostACB}
+        likePost={userlikesPostACB}
+        dislikePost={userdislikesPostACB}
+        commentText={props.model.postDetailData.comment}
+        userEntersComment={(res) => props.model.postDetailData.setComment(res)}
+        storeComment={handleSubmittedCommentACB}
+        commentModalOpen={commentModalOpen}
+        setCommentModalOpen={(state) => setCommentModalOpen(state)}
+        commentStatus={commentStatus}
+      />
     </div>
   );
 }
--- src/views/FavoritesView.jsx
+++ src/views/FavoritesView.jsx
@@ -1,9 +1,37 @@
-function FavoritesView() {
+import { useState } from "react";
+import TopRatedPostsSection from "../components/top-rated-page/TopRatedPostsSection.jsx";
+import CommentModal from "../components/modal/CommentModal.jsx";
+
+function FavoritesView(props) {
+  const [currentPost, setCurrentPost] = useState(null);
+
+  function openCommentModalACB(post) {
+    setCurrentPost(post);
+    props.setCommentModalOpen(true);
+  }
+
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col space-y-4 max-w-6xl">
+      <h2>Top Rated Pins</h2>
       <div>
-        <h1 className="text-2xl font-bold">FavoritesView</h1>
-        <p>Main content for FavoritesView...</p>
+        <TopRatedPostsSection
+          hotPosts={props.posts}
+          currentUID={props.currentUID}
+          selectPost={(id) => props.selectPost(id)}
+          likePost={(id) => props.likePost(id)}
+          dislikePost={(id) => props.dislikePost(id)}
+          commentOnCurrentPost={openCommentModalACB}
+        />
+        <CommentModal
+          post={currentPost}
+          isUserConfirmed={!!props.currentUID}
+          isOpen={props.commentModalOpen}
+          setOpen={props.setCommentModalOpen}
+          text={props.commentText}
+          userEntersComment={(res) => props.userEntersComment(res)}
+          storeComment={() => props.storeComment(currentPost)}
+          commentStatus={props.commentStatus}
+        />
       </div>
     </div>
   );
