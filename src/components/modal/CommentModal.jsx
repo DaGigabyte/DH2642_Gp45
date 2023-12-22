0a21e4ca42182ce03b96ca981f5847e495c24456
thepoppe <pontus.morin@hotmail.se>
2023-12-22 12:38:34 +0100

    Homepage view removed and sections moved to the presenter. CommentModal.jsx is now using status flag for comment

--- src/components/modal/CommentModal.jsx
+++ src/components/modal/CommentModal.jsx
@@ -2,20 +2,20 @@ import Modal from "./Modal";
 import CommentPost from "../homepage/CommentContainer";
 
 /**
- * @param {Object} props.post - The currently selected post 
+ * @param {Object} props.post - The currently selected post
  * @param {Boolean} props.isUserConfirmed - true if the user is logged in
  * @param {Function} props.userEntersComment - true if the user is logged in
  * @param {Function} props.storeComment - user wants to post the comment
- * @returns 
+ * @returns
  */
 export default function CommentModal(props) {
-
   return (
     <Modal open={props.isOpen} onOpenChange={props.setOpen}>
       <Modal.Content title="Quick Comment">
         <CommentPost
           post={props.post}
           text={props.text}
+          commentStatus={props.commentStatus}
           isUserConfirmed={props.isUserConfirmed}
           onInputChange={(res) => props.userEntersComment(res)}
           storeComment={() => props.storeComment()}
--- src/presenters/HomePresenter.jsx
+++ src/presenters/HomePresenter.jsx
@@ -1,46 +1,101 @@
 import { observer } from "mobx-react-lite";
-import { newCommentCreatedToast } from "../utils/toastify"
+import {
+  newCommentCreatedToast,
+  newCommentFailedToast,
+} from "../utils/toastify";
 
-import HomePage from "../views/HomePage";
+import { useEffect, useState } from "react";
+import TopRatedSection from "../components/homepage/toprated/TopRatedSection.jsx";
+import NewPostSection from "../components/homepage/newPosts/NewPostsSection.jsx";
+import CommentModal from "../components/modal/CommentModal.jsx";
+import SuspenseAnimation from "../components/global/SuspenseAnimation.jsx";
 
 function HomePresenter(props) {
-  let data = props.model.homePageData.data;
-
   function loadMorePostACB() {
     props.model.newestPostsData.fetchNewestPosts();
   }
+
   function userSelectsPostACB(postId) {
     props.model.postDetailData.setCurrentPostID(postId);
   }
+
   function userlikesPostACB(postId) {
     props.model.postDetailData.setCurrentPostID(postId);
     props.model.postDetailData.likePost();
   }
+
   function userdislikesPostACB(postId) {
     props.model.postDetailData.setCurrentPostID(postId);
     props.model.postDetailData.dislikePost();
   }
 
-  function handleSubmittedCommentACB(post) {
-    props.model.postDetailData.setCurrentPostID(post.id);
+  function handleSubmittedCommentACB() {
+    props.model.postDetailData.setCurrentPostID(currentPost.id);
     props.model.postDetailData.postComment();
     props.model.postDetailData.setComment("");
-    newCommentCreatedToast();
   }
 
+  const [commentModalOpen, setCommentModalOpen] = useState(false);
+  const [currentPost, setCurrentPost] = useState(null);
+
+  function openCommentModalACB(post) {
+    setCurrentPost(post);
+    setCommentModalOpen(true);
+  }
+
+  const isReady = !!(
+    props.model.topRatedPostsData.topRatedPosts.length > 0 &&
+    props.model.newestPostsData.newestPosts
+  );
+  const commentStatus = props.model.postDetailData.postCommentStatus;
+
+  // Checking for commentStatus updates
+  useEffect(() => {
+    if (commentStatus === "success") {
+      newCommentCreatedToast();
+      setCommentModalOpen(false);
+      props.model.postDetailData.setPostCommentStatus(null);
+    } else if (commentStatus === "error") {
+      newCommentFailedToast();
+      setCommentModalOpen(false);
+    }
+  }, [commentStatus, props.model.postDetailData]);
+
   return (
-    <HomePage
-      currentUID={props.model.user.uid}
-      hotPosts={props.model.topRatedPostsData.topRatedPosts}
-      newPosts={props.model.newestPostsData.newestPosts}
-      loadMorePosts={loadMorePostACB}
-      selectPost={userSelectsPostACB}
-      likePost={userlikesPostACB}
-      dislikePost={userdislikesPostACB}
-      commentText={props.model.postDetailData.comment}
-      userEntersComment={(res) => props.model.postDetailData.setComment(res)}
-      storeComment={handleSubmittedCommentACB}
-    />
+    <>
+      {isReady ? (
+        <div className="flex flex-col w-full gap-5 max-w-6xl">
+          <TopRatedSection
+            currentUID={props.model.user.uid}
+            hotPosts={props.model.topRatedPostsData.topRatedPosts}
+            selectPost={userSelectsPostACB}
+          />
+          <NewPostSection
+            newPosts={props.model.newestPostsData.newestPosts}
+            currentUID={props.model.user.uid}
+            loadMorePosts={loadMorePostACB}
+            selectPost={userSelectsPostACB}
+            likePost={userlikesPostACB}
+            dislikePost={userdislikesPostACB}
+            commentOnCurrentPost={openCommentModalACB}
+          />
+          <CommentModal
+            post={currentPost}
+            isUserConfirmed={!!props.model.user.uid}
+            isOpen={commentModalOpen}
+            setOpen={setCommentModalOpen}
+            commentStatus={commentStatus}
+            text={props.model.postDetailData.comment}
+            userEntersComment={(res) =>
+              props.model.postDetailData.setComment(res)
+            }
+            storeComment={handleSubmittedCommentACB}
+          />
+        </div>
+      ) : (
+        <SuspenseAnimation loading={!isReady} />
+      )}
+    </>
   );
 }
 
